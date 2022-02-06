359b9b93560274030af44b9b1bcb4f4e9f889839
Andrey <andrey93-15@mail.ru>
2022-02-06 17:07:14 +0200

    Domain folder for working with DB includet Entities, Interfaces

---
+++ AboutAndreyIvanov/Domain/Repositories/Abstract/IServiceItemsRepository.cs
@@ -0,0 +1,17 @@
+﻿using AboutAndreyIvanov.Domain.Entittes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutAndreyIvanov.Domain.Repositories.Abstract
+{
+    public interface IServiceItemsRepository
+    {
+        IQueryable<ServiceItem> GetTextFields();
+        ServiceItem GetTextFieldById(Guid id);
+        void SaveServiceItem(ServiceItem entity);
+        void DeleteServiceItem(Guid id);
+    }
+}
---
+++ AboutAndreyIvanov/Domain/Repositories/EntityFramework/EFServiceItemsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutAndreyIvanov.Domain.Repositories.EntityFramework
+{
+    public class EFServiceItemsRepository
+    {
+        
+    }
+}
