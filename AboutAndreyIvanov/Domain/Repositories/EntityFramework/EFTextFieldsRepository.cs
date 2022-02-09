440aae185eb52f3f2aad591114283572f3ff6f4c
Andrey <andrey93-15@mail.ru>
2022-02-09 16:20:54 +0200

    Configuration on Startup.cs, Migration

--- AboutAndreyIvanov/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
+++ AboutAndreyIvanov/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
@@ -1,14 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AboutAndreyIvanov.Domain.Entittes;
+using AboutAndreyIvanov.Domain.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AboutAndreyIvanov.Domain.Repositories.EntityFramework
 {
-    public class EFTextFieldsRepository
+    public class EFTextFieldsRepository: ITextFieldsRepository
     {
-        
+        private readonly AppDbContext _context;
+        public EFTextFieldsRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public TextField GetTextFieldByCodeWord(string codeWord)
+        {
+            return _context.TextFields.FirstOrDefault(x => x.CodeWord == codeWord);
+        }
+        public TextField GetTextFieldById(Guid id)
+        {
+            return _context.TextFields.FirstOrDefault(x => x.Id == id);
+        }
+        public IQueryable<TextField> GetTextFields()
+        {
+            return _context.TextFields;
+        }
+        public void SaveTextField(TextField entity)
+        {
+            if(entity.Id == default)   // IF a created entity has default (created now) ID 
+            {
+                _context.Entry(entity).State = EntityState.Added;   // Mark it with flag "Added"
+            }
+            else
+            {
+                _context.Entry(entity).State = EntityState.Modified; // if a entity was created before and has little change is mark it by "Modified" flag
+            }
+
+            _context.SaveChanges();
+        }
+        public void DeleteTextField(Guid id)
+        {
+            _context.TextFields.Remove(new TextField() { Id = id });  //referance to Object we need by create a new object with some ID
+            _context.SaveChanges();
+        }
 
     }
 }
--- AboutAndreyIvanov/Startup.cs
+++ AboutAndreyIvanov/Startup.cs
@@ -1,7 +1,12 @@
+using AboutAndreyIvanov.Domain;
+using AboutAndreyIvanov.Domain.Repositories.Abstract;
+using AboutAndreyIvanov.Domain.Repositories.EntityFramework;
 using AboutAndreyIvanov.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,10 +17,54 @@ namespace AboutAndreyIvanov
     {
         public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration) => Configuration = configuration;
+       
         public void ConfigureServices(IServiceCollection services)
         {
             //use config from appsetings.json
             Configuration.Bind("Project", new Config());
+
+            #region Functionality adding by services
+            
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();     //Conneting of Interface to realization (EntityFramework)
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            #endregion
+
+            #region Connection to DB with context
+
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            #endregion
+
+            #region Identity system configurations
+
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            #endregion
+
+            #region Authentication cookie settings 
+
+            services.ConfigureApplicationCookie(opts =>
+            {
+                opts.Cookie.Name = "myCompanyAuth";
+                opts.Cookie.HttpOnly = true;                        // no access on clien side
+                opts.LoginPath = "/account/login";
+                opts.AccessDeniedPath = "/account/accessdenied";
+                opts.SlidingExpiration = true;
+            });
+
+            #endregion
+
             //adding a Controllers and MVC Views support
             services.AddControllersWithViews(). // workinkg with MVC wies
                 SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddCookieTempDataProvider(); //Colibration with asp core 3.0
@@ -23,19 +72,43 @@ namespace AboutAndreyIvanov
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            //MIDDLEWARE registration procedure is important
+
+            #region Exceptions catcher in process of dev 
+
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+
+            #endregion
+
+            #region Connect static files to the app (css, jpg, js ....)
+            app.UseStaticFiles();
+            #endregion
+
+            #region Routing system
 
             app.UseRouting();
 
-            app.UseStaticFiles();
+            #endregion
+
+            #region Authentication and Authorization connect
+
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            #endregion
+
+            #region Registration of URL's we need (EndPoints)
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
+
+            #endregion
+
         }
     }
 }
